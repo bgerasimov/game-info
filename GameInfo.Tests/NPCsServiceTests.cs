b488219f0da9646d59083683c08c011f4a0cfece
Bojidar <insanerng@yahoo.com>
2018-12-26 16:49:58 +0200

    Added NPC and Profession service tests

---
+++ GameInfo.Tests/NPCsServiceTests.cs
@@ -0,0 +1,301 @@
+﻿using GameInfo.Data;
+using GameInfo.Models;
+using GameInfo.Models.InputModels;
+using GameInfo.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameInfo.Tests
+{
+    public class NPCsServiceTests
+    {
+        [Fact]
+        public void All_WithNoData_ReturnsNoData()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoNPCs_Db")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+                Assert.Equal(0, service.All().Count);
+            }
+        }
+
+        [Fact]
+        public void Add_SavesToDatabase()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "AddNPC_ToDb")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var NPCToAdd = new AddNPCInputModel()
+                { Name = "NPC Name" };
+
+                var service = new NPCsService(context, null);
+                service.Add(NPCToAdd);
+
+                var expectedNPC = new NPC()
+                { Name = NPCToAdd.Name };
+
+                Assert.NotEmpty(context.NPCs);
+                Assert.Equal(expectedNPC.Name, context.NPCs.First().Name);
+            }
+        }
+
+        [Fact]
+        public void All_WithData_ReturnsSameData()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_WithNPCs")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+
+                var NPCs = new List<NPC>
+                {
+                    new NPC() {Name = "1"},
+                    new NPC() {Name = "2"},
+                    new NPC() {Name = "3"}
+                };
+
+                context.NPCs.AddRange(NPCs);
+                context.SaveChanges();
+
+                Assert.Equal(3, service.All().Count);
+            }
+        }
+
+        [Fact]
+        public void ById_WithNoNPCs_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoNPCs_Db_ForById")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+                Assert.Null(service.ById(1));
+            }
+        }
+
+        //Does not succeed when tested along all the other tests
+        [Fact]
+        public void ById_WithNPC_ReturnNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForById_WithNPC")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+
+                var NPCToAdd = new NPC()
+                {
+                    Name = "NPC added"
+                };
+
+                context.NPCs.Add(NPCToAdd);
+                context.SaveChanges();
+
+                var NPCFromDb = service.ById(1);
+
+                Assert.Equal(NPCToAdd.Name, NPCFromDb.Name);
+            }
+        }
+
+        [Fact]
+        public void ByName_WithNoNPCs_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoNPCs_Db_ForByName")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+                Assert.Null(service.ByName("Non-existing"));
+            }
+        }
+
+        [Fact]
+        public void ByName_WithNPC_ReturnsNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForByName_WithNPC")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+
+                var NPCName = "NPC Name";
+
+                var NPC = new NPC()
+                {
+                    Name = NPCName
+                };
+
+                context.NPCs.Add(NPC);
+                context.SaveChanges();
+
+                var NPCFromDb = service.ByName(NPCName);
+
+                Assert.Equal(NPC.Name, NPCFromDb.Name);
+            }
+        }
+
+        [Fact]
+        public void Delete_NoData_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoNPCs_Db_ForDelete")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+                Assert.False(service.Delete(2));
+            }
+        }
+
+        //Does not succeed when tested along all the other tests
+        [Fact]
+        public void Delete_WithData_DeletesNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_WithNPC_ForDelete")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                context.NPCs.Add(new NPC() { Name = "ToDelete" });
+                context.SaveChanges();
+            }
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new NPCsService(context, null);
+                var result = service.Delete(1);
+
+                Assert.True(result);
+                Assert.Equal(0, context.NPCs.Count());
+            }
+        }
+
+        //Does not succeed when tested along all the other tests
+        [Fact]
+        public void AddItem_AddsToNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForAddItem_ToNPC")
+                .Options;
+
+            var itemName = "Item Name";
+
+            var npc = new NPC() { Name = "NPC Name" };
+            var item = new Item() { Name = itemName, AcquiredFrom = "None", Usage = "None" };
+
+            using (var context = new GameInfoContext(options))
+            {
+                context.NPCs.Add(npc);
+                context.Items.Add(item);
+                context.SaveChanges();
+
+                var model = new AddItemToNPCInputModel() { NPCId = 1, ItemName = itemName };
+
+                var service = new NPCsService(context, new ItemsService(context));
+
+                Assert.True(service.AddItemToNPC(model));
+            }
+        }
+
+        [Fact]
+        public void AddQuest_AddsToNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForAddQuest_ToNPC")
+                .Options;
+
+            var npc = new NPC() { Name = "NPC QuestAdd" };
+            var quest = new Quest() { Title = "QuestTitle", CompletionCondition = "None" };
+
+            using (var context = new GameInfoContext(options))
+            {
+                context.NPCs.Add(npc);
+                context.Quests.Add(quest);
+                context.SaveChanges();
+
+                var model = new AddQuestToNPCInputModel() { NPCId = 1 };
+
+                var service = new NPCsService(context, null);
+
+                Assert.True(service.AddQuestToNPC(model, quest));
+            }
+        }
+
+        [Fact]
+        public async Task RemoveItem_RemovesFromNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForRemoveItem_FromNPC")
+                .Options;
+
+            var npc = new NPC() { Name = "NPC RemoveItem" };
+            var item = new Item() { Name = "Item to remove" };
+
+            using (var context = new GameInfoContext(options))
+            {
+                npc.Loot.Add(item);
+                context.NPCs.Add(npc);
+                context.SaveChanges();
+
+                var service = new NPCsService(context, null);
+
+                await service.RemoveItem(npc, item);
+
+                var NPCFromDb = context.NPCs.First();
+
+                Assert.True(NPCFromDb.Loot.Count == 0);
+            }
+        }
+
+        [Fact]
+        public async Task RemoveQuest_RemovesFromNPC()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_ForRemoveQuest_FromNPC")
+                .Options;
+
+            var npc = new NPC() { Name = "NPC RemoveQuest" };
+            var quest = new Quest() { Title = "To remove" };
+
+            using (var context = new GameInfoContext(options))
+            {
+                npc.Quests.Add(quest);
+                context.NPCs.Add(npc);
+                context.SaveChanges();
+
+                var service = new NPCsService(context, null);
+
+                await service.RemoveQuest(npc, quest);
+
+                var NPCFromDb = context.NPCs.First();
+
+                Assert.True(NPCFromDb.Quests.Count == 0);
+            }
+        }
+    }
+}
---
+++ GameInfo.Tests/ProfessionsServiceTests.cs
@@ -0,0 +1,251 @@
+﻿using GameInfo.Data;
+using GameInfo.Models;
+using GameInfo.Models.Enums;
+using GameInfo.Models.InputModels;
+using GameInfo.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameInfo.Tests
+{
+    public class ProfessionsServiceTests
+    {
+        [Fact]
+        public void All_WithNoData_ReturnsNoData()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoProfessions_Db")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+                Assert.Equal(0, service.All().Count);
+            }
+        }
+
+        [Fact]
+        public void Add_SavesToDatabase()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "AddProfession_ToDb")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var professionToAdd = new AddProfessionInputModel()
+                {
+                    Name = "ProfessionAdd",
+                    UsableWeaponType = WeaponType.Axe.ToString(),
+                    CombatType = CombatType.Melee.ToString(),
+                    ClassRole = ClassRole.Damage.ToString()
+                };
+
+                var service = new ProfessionsService(context);
+                service.Add(professionToAdd);
+
+                var expectedProfession = new Profession()
+                {
+                    Name = professionToAdd.Name
+                };
+
+                Assert.NotEmpty(context.Professions);
+                Assert.Equal(expectedProfession.Name, context.Professions.First().Name);
+            }
+        }
+
+        [Fact]
+        public void All_WithData_ReturnsSameData()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "Db_WithProfessions")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+
+                var professions = new List<Profession>
+                {
+                    new Profession() {Name = "1", },
+                    new Profession() {Name = "2", },
+                    new Profession() {Name = "3", }
+                };
+
+                context.Professions.AddRange(professions);
+                context.SaveChanges();
+
+                Assert.Equal(3, service.All().Count);
+            }
+        }
+
+        [Fact]
+        public void ById_WithNoProfessions_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoProfessions_ForById")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+                Assert.Null(service.ById(1));
+            }
+        }
+
+        //Does not succeed when tested along all the other tests
+        [Fact]
+        public void ById_WithProfession_ReturnsProfession()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "WithProfession_ForById")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+
+                var professionToAdd = new Profession()
+                {
+                    Name = "Profession"
+                };
+
+                context.Professions.Add(professionToAdd);
+                context.SaveChanges();
+
+                var professionFromDb = service.ById(1);
+
+                Assert.Equal(professionToAdd.Name, professionFromDb.Name);
+            }
+        }
+
+        [Fact]
+        public void ByName_WithNoProfessions_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoProfessions_ForByName")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+                Assert.Null(service.ByName("Non-existing"));
+            }
+        }
+
+        [Fact]
+        public void ByName_WithProfession_ReturnsProfession()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "ForByName_WithProfession")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+
+                var professionName = "Prof Name";
+
+                var profession = new Profession()
+                {
+                    Name = professionName
+                };
+
+                context.Professions.Add(profession);
+                context.SaveChanges();
+
+                var professionFromDb = service.ByName(professionName);
+
+                Assert.Equal(profession.Name, professionFromDb.Name);
+            }
+        }
+
+        [Fact]
+        public void Delete_NoData_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "NoProfessions_ForDelete")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+                Assert.False(service.Delete(2));
+            }
+        }
+
+        //Does not succeed when tested along all the other tests
+        [Fact]
+        public void Delete_WithData_DeletesProfession()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "WithProfession_ForDelete")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                context.Professions.Add(new Profession() { Name = "ToDelete" });
+                context.SaveChanges();
+            }
+
+            using (var context = new GameInfoContext(options))
+            {
+                var service = new ProfessionsService(context);
+                var result = service.Delete(1);
+
+                Assert.True(result);
+                Assert.Equal(0, context.Professions.Count());
+            }
+        }
+
+        [Fact]
+        public async Task RemoveRace_WithData_RemovesFromProfession()
+        {
+            var options = new DbContextOptionsBuilder<GameInfoContext>()
+                .UseInMemoryDatabase(databaseName: "WithRaceInProf_ForRemoveRace")
+                .Options;
+
+            using (var context = new GameInfoContext(options))
+            {
+                var profession = new Profession()
+                {
+                    Name = "ProfWithRace"
+                };
+
+                var race = new Race()
+                {
+                    Name = "RaceWithProf"
+                };
+
+                context.Professions.Add(profession);
+                context.Races.Add(race);
+                await context.SaveChangesAsync();
+
+                var profFromDb = context.Professions.First();
+                var raceFromDb = context.Races.First();
+
+                var raceProfession = new RaceProfession()
+                {
+                    Race = raceFromDb,
+                    RaceId = raceFromDb.Id,
+                    Profession = profFromDb,
+                    ProfessionId = profFromDb.Id
+                };
+
+                profFromDb.Races.Add(raceProfession);
+
+                await context.SaveChangesAsync();
+
+                var service = new ProfessionsService(context);
+                await service.RemoveRace(profFromDb, raceFromDb);
+
+                Assert.DoesNotContain(raceProfession, profFromDb.Races);
+            }
+        }
+    }
+}
